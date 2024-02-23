abf2d254b7307d7c7daf29cd4caeba2fb978a087
Gian Baez <gianbaez@Gians-MacBook-Air.local>
2024-02-23 16:57:47 -0500

    hdh

---
+++ mywork/madlib.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+clear
+echo "Let's build a mad-lib!"
+
+read -p "1. Please give me an adjective: " ADJ1
+read -p "2. Please give me a noun: " N1
+read -p "3. Please give me a verb: " V1
+read -p "4. Please give me an adverb: " AV1
+read -p "5. Please give me an adjective : " ADJ2
+read -p "6. Please give me a noun: " N2
+read -p "7. Please give me a verb: " V2
+read -p "8. Please give me an adverb: " AV2
+
+
+echo "Once upon a time a $ADJ1 boy $V1 $AV1 into the $N1 where he $AV2 $V2 upon an $ADJ2 $N2."
+
